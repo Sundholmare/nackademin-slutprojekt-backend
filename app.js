be324c22ae6eb5273aea637246176ead1062061a
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-05-18 19:50:47 +0200

    register router

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const registerRouter = require('./router/register')
 
-app.use( express.static('public') )
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('/', registerRouter)
+
 
 module.exports = app
---
+++ router/register.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+router.post('/api/register', (req, res) => {
+    console.log(req)
+})
+
+
+module.exports = router;
