107c82eb17d984831c243668ffad1782e38f7a3d
Marcus Järveskog <abr.metalcore98@gmail.com>
2021-05-19 10:46:07 +0200

    connected DB

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 
-app.use( express.static('public') )
+app.use(express.static('public'));
 
-module.exports = app
+// connect to db
+mongoose.connect(
+  'mongodb+srv://Eiser:eiser1@cluster0.wafzm.mongodb.net/SinusDB?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('connected to db')
+);
+
+module.exports = app;
