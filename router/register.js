7668af400d061bd06cb40a3bd9a55741218f8014
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-05-19 21:58:19 +0200

    Update register.js

--- router/register.js
+++ router/register.js
@@ -7,40 +7,50 @@ const saltRounds = 10
 // const cookieParser = require('cookie-parser')
 // app.use(cookieParser())
 
-// 1-behöver vi kolla om man är inloggad med hjälp av cookie ?
-//2-kolla om användaren finns redan?
-//3-email i moduel behöver fixas 
-// 4-zip i moduel behöver vara number
-// 5-orderHistory ska kopplas vid register eller inte
+
+const checkRepeatPassword = (pass1, pass2) => {
+    if (pass1 === pass2) return true
+}
 
 
 
-router.post('/register', (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        if (err) res.json(err)
-        else {
-            const newUser = new User({
-                _id: new mongoose.Types.ObjectId(),
-                role: 'customer',
-                email: req.body.email,
-                password: hash,
-                name: req.body.name,
-                adress: {
-                    street: req.body.adress.street,
-                    zip: req.body.adress.zip,
-                    city: req.body.adress.city,
-                }
-            })
-            newUser.save((err) => {
-                if (err) {
-                    console.log(err)
-                    res.json(err)
-                }
-                else {
-                    console.log(newUser)
-                    res.json(newUser)
-                }
-            })
+router.post('/register', async (req, res) => {
+    User.findOne({ $or: [{ name: req.body.name }, { email: req.body.email }] }, function (err, user) {
+        if (err) console.log(err)
+        if (user) {
+            console.log(`This name: ${user.name} or this email: ${user.email} is already exists`)
+            res.end();
+        } else {
+            console.log(`This is a new user`)
+            if (checkRepeatPassword(req.body.password, req.body.repeatPassword)) {
+                bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+                    if (err) res.json(err)
+                    else {
+                        const newUser = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            role: 'customer',
+                            email: req.body.email,
+                            password: hash,
+                            name: req.body.name,
+                            adress: {
+                                street: req.body.adress.street,
+                                zip: req.body.adress.zip,
+                                city: req.body.adress.city,
+                            }
+                        })
+                        newUser.save((err) => {
+                            if (err) {
+                                console.log(err)
+                                res.json(err)
+                            }
+                            else {
+                                console.log(newUser)
+                                res.json(newUser)
+                            }
+                        })
+                    }
+                })
+            } else console.log(`Different passwords`)
         }
     })
 })
