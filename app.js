ff18e51a95ba86f3f5c723bcf27464e436d028f8
Marcus Järveskog <abr.metalcore98@gmail.com>
2021-05-19 11:17:56 +0200

    added dotenv for DB connection

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 app.use(express.static('public'));
 
 // connect to db
 mongoose.connect(
-  'mongodb+srv://Eiser:eiser1@cluster0.wafzm.mongodb.net/SinusDB?retryWrites=true&w=majority',
+  process.env.DB_CONNECT,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log('connected to db')
 );
