e6e5af7d65aa7e1ccad61e4df24bb9d3dbe72101
Namir Wesaf <Namir_wesaf@hotmail.com>
2021-05-24 18:10:13 +0200

    Auth & Token

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const register_Router = require('./router/register')
 const createProduct_Router = require('./router/createProduct')
 const productsRouter = require('./router/products');
+const authRouter = require('./router/auth')
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -14,10 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', register_Router)
 app.use('/api', createProduct_Router)
-
-
-
-app.use('/api', productsRouter);
+app.use('/api', authRouter)
+app.use('/api', productsRouter)
 
 
 
---
+++ middleware/loggedIn.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+/*
+* Verifierar att token är äkta
+* Används EJ just nu
+*/
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, "hemligfras123treettfemsju" as string, (err: any, user: any) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
